0eb05277979383688b629cb73d8c2bcc811ee967
shilin.guo <552002499@qq.com>
2016-07-07 23:12:32 +0800

    Add DM9000 config  Changes to be committed: 	modified:   include/configs/smdk2440.h

--- u-boot-2016.05/include/configs/smdk2440.h
+++ u-boot-2016.05/include/configs/smdk2440.h
@@ -35,9 +35,11 @@
 /*
  * Hardware drivers
  */
-#define CONFIG_CS8900		/* we have a CS8900 on-board */
-#define CONFIG_CS8900_BASE	0x19000300
-#define CONFIG_CS8900_BUS16	/* the Linux driver does accesses as shorts */
+#define CONFIG_DRIVER_DM9000
+#define CONFIG_DM9000_NO_SROM
+#define CONFIG_DM9000_BASE 0x20000000
+#define DM9000_IO CONFIG_DM9000_BASE
+#define DM9000_DATA (CONFIG_DM9000_BASE+4)
 
 /*
  * select serial console configuration
